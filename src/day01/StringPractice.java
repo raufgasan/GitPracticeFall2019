ac2debd0cf84ff3fb20e3ab5f3d4671e220eb486
raufgasan <raufgasan@gmail.com>
2020-02-29 23:39:07 -0800

    new one

--- src/day01/StringPractice.java
+++ src/day01/StringPractice.java
@@ -4,6 +4,9 @@ public class StringPractice {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("one more time ");
+        String str = "name";
+        System.out.println("str = " + str);
 
     }
 }
